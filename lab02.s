1df47807b4e38e1604674777f18efec14ec39e22
panagiotisskarmoutsos <114870585+panagiotisskarmoutsos@users.noreply.github.com>
2024-10-16 21:25:32 +0300

    Add files via upload

--- lab02.s
+++ lab02.s
@@ -12,6 +12,16 @@ prog:
 #-----------------------------
 # Write your code here!
 # Do not remove the prog label or write code above it!
+add s0, zero, zero
+loop:
+beq a1, zero, done
+lw t0, 0(a0)
+bne a2, t0, outif
+add s0, a0, zero
+outif:
+addi a0, a0, 4
+addi a1, a1, -1
+j loop
 #-----------------------------
 done:
     addi a7, zero, 10 
